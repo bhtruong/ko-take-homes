95ea4532bdaafaf19949d1b5c9704363d876b346
Brian Truong <bstruong1@gmail.com>
2017-10-02 15:46:21 -0700

    Step 3: Display movie's release year next to title

--- front-end/movies-evan-likes/src/components/movie-list-item.js
+++ front-end/movies-evan-likes/src/components/movie-list-item.js
@@ -3,11 +3,14 @@ import React from 'react'
 const MovieListItem = (props) => {
   const url = props.movie.url
   const movieTitle = props.movie.title
+  const movieYear = props.movie.year
+
   return (
     <li>
       <a href={url}>
         {movieTitle}
       </a>
+      { ' (' + movieYear + ')' }
     </li>
   )
 }
